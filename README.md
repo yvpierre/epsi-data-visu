e038931faa9bd47beb0f27d11a767b07afefa644
Pierre <pierre.yvenou@orange.fr>
2024-02-28 09:16:15 +0100

    requirements.txt file

---
+++ README.md
@@ -0,0 +1,10 @@
+# EPSI Data visualisation
+Application requires Python3
+## Install depedencies 
+```
+pip3 install -r ./requirements.txt
+```
+## Start app
+```
+python3 app.py
+```
--- indicateursNationaux.py
+++ indicateursNationaux.py
@@ -1,1 +1,6 @@
 import matplotlib.pyplot as plt
+
+# Discrete distribution as horizontal chart
+# Based on the "indicateurs_nationaux_apprentissage_2020_2021"
+def horizontal_chart(sourceFile):
+    
